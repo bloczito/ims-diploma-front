d7d8630d942bc9f4afe8ba6d7cd029a4e35a2cd8
Bartosz Krawiec <50678871+sosnowski66@users.noreply.github.com>
2021-05-09 14:28:13 +0200

    -

--- src/views/ProductsView/ProductsView.js
+++ src/views/ProductsView/ProductsView.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { productService } from "../../_service";
-import { Button, CircularProgress, Container, Grid, Typography } from "@material-ui/core";
+import { Button, Container, Grid, Typography } from "@material-ui/core";
 import TablePagination from "../../components/TablePagination/TablePagination";
 import ProductsTable from "../../components/ProductsTable/ProductsTable";
 import OverlaySpinner from "../../components/OverlaySpinner/OverlaySpinner";
@@ -62,59 +62,56 @@ const ProductsView = () => {
 
     return (
         <>
-            {isLoading
-                ? <CircularProgress />
-                : (
-                    <Container className={styles.wrapper}>
-                        <Grid container alignContent="space-between" direction="row">
-                            <Grid item md={4}>
-                                <Button variant="contained"
-                                        color="primary"
-                                        onClick={() => setIsModalOpen(true)}
-                                        disableElevation>
-                                    Dodaj produkt
-                                </Button>
-                            </Grid>
-                            <Grid item md={4}>
-                                <Typography variant="h3" gutterBottom>
-                                    Produkty
-                                </Typography>
-                            </Grid>
-                                <Grid item md={4}>
-                                    <Grid container justify="flex-end">
-                                        <TablePagination
-                                            totalElements={totalElements}
-                                            rowsPerPage={rowsPerPage}
-                                            pageNr={page}
-                                            handleChangePage={handleChangePage}
-                                            handleChangeRowsPerPage={handleChangeRowsPerPage}
-                                        />
-                                    </Grid>
-                                </Grid>
-                        </Grid>
-
-                        <Grid container>
-                            <Grid item md>
-                                <ProductsTable
-                                    products={products}
-                                    page={page}
+            <OverlaySpinner isActive={isLoading}/>
+            <Container className={styles.wrapper}>
+                <Grid container alignContent="space-between" direction="row">
+                    <Grid item md={4}>
+                        <Button variant="contained"
+                                color="primary"
+                                onClick={() => setIsModalOpen(true)}
+                                disableElevation>
+                            Dodaj produkt
+                        </Button>
+                    </Grid>
+                    <Grid item md={4}>
+                        <Typography variant="h3" gutterBottom>
+                            Produkty
+                        </Typography>
+                    </Grid>
+                        <Grid item md={4}>
+                            <Grid container justify="flex-end">
+                                <TablePagination
+                                    totalElements={totalElements}
                                     rowsPerPage={rowsPerPage}
+                                    pageNr={page}
                                     handleChangePage={handleChangePage}
                                     handleChangeRowsPerPage={handleChangeRowsPerPage}
-                                    totalElements={totalElements}
                                 />
                             </Grid>
                         </Grid>
+                </Grid>
 
-                    </Container>
-                )
-            }
+                <Grid container>
+                    <Grid item md>
+                        <ProductsTable
+                            products={products}
+                            page={page}
+                            rowsPerPage={rowsPerPage}
+                            handleChangePage={handleChangePage}
+                            handleChangeRowsPerPage={handleChangeRowsPerPage}
+                            totalElements={totalElements}
+                        />
+                    </Grid>
+                </Grid>
+
+            </Container>
+
+
             <NewProductModal
                 handleNewProductSubmit={handleNewProductSubmit}
                 isOpen={isModalOpen}
                 handleModalClose={handleModalClos}
             />
-            <OverlaySpinner isActive={true}/>
         </>
     )
 
